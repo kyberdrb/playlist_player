637c0dd45e8b7af478f0d7fb88e1d16066787727
kyberdrb <kyberdrb@gmail.com>
2020-05-17 12:43:44 +0200

    Prepare for refactoring to classes

--- playlist_player.py
+++ playlist_player.py
@@ -1,57 +1,95 @@
-import random
-import subprocess
-import time
+import sys
+def play():
+    # PlaylistLoader - empty ctor/ctor(PlaylistFile)/ctor(YoutubePlaylist) - polymorphism? overload? - first overload, then polymorphism
+    playlistFile = "dnb_instrumental_no_vocals.m3u8"
+    if len(sys.argv) >= 2:
+        playlistFile = sys.argv[1]
+        print("Argument provided:\t%s"% playlistFile)
 
-songURLs = list()
+    # PlaylistLoader - load(PlaylistFile playlistFile) - overload
+    playlistItems = list()
 
-with open("dnb_instrumental_no_vocals.m3u8", "r") as playlistFile:
-    for line in playlistFile:
-        if "youtube" in line:
-            line = line.rstrip("\n")
-            songURLs.append(line)
+    with open(playlistFile, "r") as playlist:
+        print("Reading file:\t%s"% playlistFile)
+        for line in playlist:
+            if "www" in line:
+                line = line.rstrip("\n")
+                playlistItems.append(line)
 
-random.shuffle(songURLs)
+    # PlaylistLoader - load(YoutubePlaylist youtubePlaylist) - overload
+        import subprocess
+        #youtube-dl --flat-playlist --dump-json https://www.youtube.com/playlist?list=UUREvOFg1F4bfZZqdmIJknrg | cut -d' ' -f4 | sed 's/[",]//g' | sed "s/^/https:\/\/www\.youtube\.com\/watch\?v=/g"
 
-for songURL in songURLs:
-    print(songURL)
+    # Playlist - shuffle()
+    import random
+    random.shuffle(playlistItems)
 
-print()
+    # Playlist - getAllPlaylistItems()
+    for songURL in playlistItems:
+        print(songURL)
 
-for songURL in songURLs:
-    print(songURL)
-    p = subprocess.Popen(
-        ["vlc", "--play-and-exit", "--playlist-autostart", "--no-video", "--qt-start-minimized", songURL], 
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print()
 
-    paddingSize = 0
-    step = 1
-    while p.poll() == None:
-        padding = str()
-        for eachUnit in range(paddingSize):
-            padding += ' '
+    # PlaylistPlayer - play(Playlist)
+    import time
+    import subprocess
+    for songURL in playlistItems:
+        print(songURL)
 
-        if paddingSize == 0:
-            sign = "("
-            step = 1
+        # Option "--play-to-the-end": start the player in a detached thread; the thread will terminate after 
+        #  the playlist item will go to the end, instead of terminating the player with the script at 'CTRL + C', i.e. KeyboardInterrupt exception
+        p = subprocess.Popen(
+            ["vlc", "--play-and-exit", "--playlist-autostart", "--no-video", "--qt-start-minimized", songURL], 
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        if paddingSize == 4:
-            sign = ")"
-            step = -1
+        # WaveDrawer - constructor
+        paddingSize = 0
+        step = 1
 
-        if paddingSize != 0 and paddingSize != 4 and step == 1:
-            sign = "\\"
+        # WaveDrawer - draw
+        while p.poll() == None:
+            padding = str()
+            for eachUnit in range(paddingSize):
+                padding += ' '
 
-        if paddingSize != 0 and paddingSize != 4 and step == -1:
-            sign = "/"
+            # if wave.hitLeftBorder():
+            if paddingSize == 0:
+                # wave.bounceOffFromLeftBorder()
+                sign = "("
+                step = 1
 
-        print(padding + sign)
-        paddingSize += step
-        time.sleep(1)
+            # if wave.hitRightBorder():
+            if paddingSize == 4:
+                # wave.bounceOffFromRightBorder()
+                sign = ")"
+                step = -1
 
+            # Encapsulate compound condition into a separate boolean function
+            # if wave.goesForward():
+            if paddingSize != 0 and paddingSize != 4 and step == 1:
+                sign = "\\"
 
-print()
-print("============================================")
-print("Playback finished.")
-print("============================================")
-print()
+            # Encapsulate compound condition into a separate boolean function
+            # if waveDrawer.goesBackward():
+            if paddingSize != 0 and paddingSize != 4 and step == -1:
+                sign = "/"
+
+            print(padding + sign)
+            paddingSize += step
+
+            # PlaylistPlayer - play(Playlist) cont.
+            time.sleep(1)
+
+    # Option "--loop / --no-loop": when specified, shuffle again and play, otherwise stop at the end of the playlist
+    #  default: enabled (--loop)
+    print()
+    print("============================================")
+    print("Playback finished.")
+    print("============================================")
+    print()
+
+# first encapsulate the rest of this file into a 'main()' method
+# then add launch condition
+if __name__ == "__main__":
+    play()
 
